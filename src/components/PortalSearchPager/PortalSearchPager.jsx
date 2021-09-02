6a91f0c0ab17b6e4deaf4ecfdb8eae0a08702ea7
alex-1pro <pro1.alex.b@gmail.com>
2021-09-02 13:38:15 +0300

    adding font-family assistant-light

--- src/components/PortalSearchPager/PortalSearchPager.jsx
+++ src/components/PortalSearchPager/PortalSearchPager.jsx
@@ -3,7 +3,7 @@ import './PortalSearchPager.css'
 function PortalSearchPager({placeholder ,pages,handleSearch, pageChange}) {
     return (
         <div className="c-search">
-          <input className="search-input" type="text"></input>
+          <input className="search-input" type="text" placeholder={placeholder}></input>
         </div>
     );
 }
