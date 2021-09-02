01a014ff45da5224cacc6f8b1499eb008725a173
alex-1pro <pro1.alex.b@gmail.com>
2021-09-02 15:35:18 +0300

    adding arrows  and page counter to in put

--- src/components/PortalSearchPager/PortalSearchPager.jsx
+++ src/components/PortalSearchPager/PortalSearchPager.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import './PortalSearchPager.css'
-function PortalSearchPager({placeholder ,pages,handleSearch, pageChange}) {
+import arrowImg from '../../assets/images/arrow/arrow_down.png'
+function PortalSearchPager({placeholder ,pages,currentPage,handleSearch, pageChange}) {
+    function onArrowRight(){
+        if(currentPage>1){
+            pageChange(currentPage-1);
+        }
+    }
+
+  function onArrowLeft(){
+        if(currentPage<pages){
+            pageChange(currentPage+1);
+        }
+    }
+
+    function handleKeyDown(e,text){
+        if(e.key==='Enter'){
+            // handleSearch(text);
+            console.log(text);
+        }
+    }
     return (
-        <div className="c-search">
-          <input className="search-input" type="text" placeholder={placeholder}></input>
+        <div className="c-search-pager">
+          <input className="search-input" type="text" placeholder={placeholder} ></input>
+          <div className="pages-container">
+                <img className="img-arrow-right" src={arrowImg} onClick={onArrowRight} alt="" />  
+                <span>{currentPage}</span> 
+               {pages>0? <img className="img-arrow-left" src={arrowImg} onClick={onArrowLeft} alt="" />:null   }
+          </div>
         </div>
     );
 }
