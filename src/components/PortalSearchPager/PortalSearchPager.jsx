b992d3556f8b280cdbe23425f7d33dc970a9c041
alex-1pro <pro1.alex.b@gmail.com>
2021-09-04 14:34:31 +0300

    adding functionality to component

--- src/components/PortalSearchPager/PortalSearchPager.jsx
+++ src/components/PortalSearchPager/PortalSearchPager.jsx
@@ -1,33 +1,32 @@
 import React from 'react';
 import './PortalSearchPager.css'
 import arrowImg from '../../assets/images/arrow/arrow_down.png'
-function PortalSearchPager({placeholder ,pages,currentPage,handleSearch, pageChange}) {
-    function onArrowRight(){
-        if(currentPage>1){
-            pageChange(currentPage-1);
+import { useState } from 'react';
+function PortalSearchPager({ placeholder, pages, currentPage, onSearchChange, onPageChange, searchText }) {
+
+    const [textValue, setTextValue] = useState(searchText);
+
+    const onArrowRight = () => {
+        if (currentPage > 1) {
+            onPageChange(currentPage - 1);
+        }
+    }
+    const onArrowLeft = () => {
+        if (currentPage < pages) {
+            onPageChange(currentPage + 1);
         }
     }
 
-  function onArrowLeft(){
-        if(currentPage<pages){
-            pageChange(currentPage+1);
-        }
-    }
-
-    function handleKeyDown(e,text){
-        if(e.key==='Enter'){
-            // handleSearch(text);
-            console.log(text);
-        }
-    }
     return (
         <div className="c-search-pager">
-          <input className="search-input" type="text" placeholder={placeholder} ></input>
-          <div className="pages-container">
-                <img className="img-arrow-right" src={arrowImg} onClick={onArrowRight} alt="" />  
-                <span>{currentPage}</span> 
-               {pages>0? <img className="img-arrow-left" src={arrowImg} onClick={onArrowLeft} alt="" />:null   }
-          </div>
+            <form onSubmit={() => onSearchChange(textValue)}>
+                <input className="search-input" type="text" placeholder={placeholder} value={textValue} onChange={e => setTextValue(e.target.value)}></input>
+            </form>
+            {pages ? <div className="pages-container">
+                <img className={`img-arrow-right ${currentPage === 1 ? " opacity" : ""}`} src={arrowImg} onClick={onArrowRight} alt="" />
+                <span className="pages-num ">{currentPage}</span>
+                <img className={`img-arrow-left ${currentPage === pages ? " opacity" : ""}`} src={arrowImg} onClick={onArrowLeft} alt="" />
+            </div> : null}
         </div>
     );
 }
