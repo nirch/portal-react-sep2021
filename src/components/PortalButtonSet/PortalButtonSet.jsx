d5babaf3f69d71bb4c2ea4d178d9121f67adfe57
alex-1pro <pro1.alex.b@gmail.com>
2021-09-12 10:47:56 +0300

    adding code review changing

--- src/components/PortalButtonSet/PortalButtonSet.jsx
+++ src/components/PortalButtonSet/PortalButtonSet.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import './PortalButtonSet.css';
 
-//prop shadowBox get 'top' ,'bottom' or emty string value
-function PortalButtonSet({ buttons, handleClick, pressed, shadowBox }) {
-    const btns = buttons.map(btn => <div key={btn.label} className={`${btn.label === pressed.label ? "btn-pressed" : "single-btn"}`} onClick={() => handleClick(btn)}>{btn.label}</div>);
+/** ======= Props =======
+ * - buttons : get array of objects {key:key,lable:lable}
+ * - onClick : function return object that pressed on
+ * - pressedKey : get pressed button
+ * - shadowBox: get 'top' ,'bottom' or emty string value
+ *  
+ * ======= Example =======
+ *  
+ * const buttons = [{ label: "פעילים עובדים", key: 0 }, { label: "לא פיעילים", key: 2 }, { label: "כולם", key: 3 }];
+ *  <PortalButtonSet buttons={buttons} onClick={ btn=> alert(btn.label)} pressedKey={buttons[0].key} shadowBox={"top"}/>
+ */
+
+function PortalButtonSet({ buttons, onClick, pressedKey, shadowBox }) {
+    const btns = buttons.map(btn =>
+        <div key={btn.key}
+            className={`${btn.key === pressedKey ? "btn-pressed" : ""}`}
+            onClick={() => onClick(btn)} >
+            {btn.label}
+        </div>);
 
     return (
         <div className="c-button-set">
