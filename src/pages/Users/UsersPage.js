d947a5c726ace4fef10ea84309298b434dd5b5b7
alex-1pro <pro1.alex.b@gmail.com>
2021-09-12 11:41:52 +0300

    adding PortalSearchPager component

--- src/pages/Users/UsersPage.js
+++ src/pages/Users/UsersPage.js
@@ -1,21 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './UsersPage.css'
 import PortalNavbar from '../../components/navbar/PortalNavbar';
+import PortalSearchPager from '../../components/PortalSearchPager/PortalSearchPager';
 import ActiveUserContext from '../../shared/activeUserContext';
 import { Redirect } from 'react-router-dom'
 
 const UsersPage = (props) => {
     const { handleLogout } = props;
     const activeUser = useContext(ActiveUserContext);
-    
+
+    // pages, currentPage, onSearchChange, onPageChange 
+    const [pages, setPages] = useState(5);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [textSearch, setTextSearch] = useState("");
+
     if (!activeUser) {
         return <Redirect to='/' />
     }
-    
+
     return (
         <div className="p-users">
-            <PortalNavbar handleLogout={handleLogout}/>
-            <h1>משתמשים</h1>
+            <PortalNavbar handleLogout={handleLogout} />
+            <div className="c-search">
+                <PortalSearchPager placeholder={"חיפוש עובד"}
+                    pages={pages} currentPage={currentPage}
+                    onSearchChange={setTextSearch}
+                    onPageChange={setCurrentPage} />
+            </div>
         </div>
     );
 }
