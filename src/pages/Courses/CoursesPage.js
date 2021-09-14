c79d1768065108cb828e945e9a123169985724aa
Eran Tzinamon <tzinamon@gmail.com>
2021-09-14 11:45:16 +0300

    connected portalButtonSet component

--- src/pages/Courses/CoursesPage.js
+++ src/pages/Courses/CoursesPage.js
@@ -11,14 +11,15 @@ import PortalButtonSet from '../../components/PortalButtonSet/PortalButtonSet';
 const CoursesPage = (props) => {
     const { handleLogout } = props;
     const activeUser = useContext(ActiveUserContext);
+    const headers = [{key: "subname", header: "שם קורס מקוצר"}, {key: "project", header: "פרויקט"}, {key: "teachers", header: "מדריך"}];
 
     const [pages, setPages] = React.useState(10);
     const [currentPage, setCurrentPage] = React.useState(0);
-    const [pressedKey, setPressedKey] = React.useState(0);
+    const [pressedKey, setPressedKey] = React.useState(1);
     const [searchQuery, setSeachQuery] = React.useState ("");
-    const [coursesData, setCoursesData] =React.useState ([]);
-
-
+    const [coursesData, setCoursesData] = React.useState ([]);
+    const buttons = [{label: "קורסים פעילים", key: 1 }, { label: "לא פעילים", key: 0 }];
+    const [shadowBox, setShadowbox] = React.useState ("top");
 
     useEffect(()=>{
         const data = {coursestatus: pressedKey, desc: false, page: currentPage, search: searchQuery, sorting: "courseid"};
@@ -27,13 +28,13 @@ const CoursesPage = (props) => {
             if (res.data.error) {
                 alert("error in data");
             } else {
-                setCoursesData(res.data);
+                setCoursesData(res.data.courses);
             }    console.log(res.data);
         })
      }, [currentPage, pressedKey]);
 
-    function buttonclick(){
-
+    function buttonClick(btn){
+        setPressedKey(btn.key);
     }
 
     function clickedEntry (user){
@@ -50,9 +51,8 @@ const CoursesPage = (props) => {
             <PortalNavbar handleLogout={handleLogout}/>
             <h1>קורסים</h1>
             {/* <PortalSearchPager placeholder={"חיפוש קורס"} pages={pages} currentPage={currentPage} onSearchChange={querySearch} onPageChange={pageChanger} />  */}
-            {/* <PortalTable headers={headers} data={data} onClick={clickedEntry}/> */}
-            {/*, , shadowBox  */}
-            {/* <PortalButtonSet buttons={buttons} onClick={buttonClick} pressedKey={pressedKey} /> */}
+            <PortalTable headers={headers} data={coursesData} onClick={clickedEntry}/>
+            <PortalButtonSet buttons={buttons} onClick={buttonClick} pressedKey={pressedKey} shadowBox={shadowBox} />
 
         </div>
     );
